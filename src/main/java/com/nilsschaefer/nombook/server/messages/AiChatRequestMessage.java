9afa9ac56e12edf3042aeebfc63af250fc4d6d78
nilschae <development.nils.schaefer@gmail.com>
2026-05-03 15:35:44 +0200

    Add AiChatStatusMessage and update protocol for intermediate updates

--- src/main/java/com/nilsschaefer/nombook/server/messages/AiChatRequestMessage.java
+++ src/main/java/com/nilsschaefer/nombook/server/messages/AiChatRequestMessage.java
@@ -2,13 +2,19 @@ package com.nilsschaefer.nombook.server.messages;
 
 public class AiChatRequestMessage extends Message {
     private String text;
+    private String conversationId;
 
-    public AiChatRequestMessage(String from, String text) {
+    public AiChatRequestMessage(String from, String text, String conversationId) {
         super(from, MessageType.AI_CHAT_REQUEST);
         this.text = text;
+        this.conversationId = conversationId;
     }
 
     public String getText() {
         return text;
+    }
+
+    public String getConversationId() {
+        return conversationId;
     }
 }
