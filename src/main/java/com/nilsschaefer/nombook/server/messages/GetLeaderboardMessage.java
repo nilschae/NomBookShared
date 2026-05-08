b37a66942fdd18f71cb60e0d11bd7c6980016b01
nilschae <development.nils.schaefer@gmail.com>
2026-05-08 10:52:56 +0200

    Added global flag to GetLeaderboardMessage

--- src/main/java/com/nilsschaefer/nombook/server/messages/GetLeaderboardMessage.java
+++ src/main/java/com/nilsschaefer/nombook/server/messages/GetLeaderboardMessage.java
@@ -1,7 +1,18 @@
 package com.nilsschaefer.nombook.server.messages;
 
 public class GetLeaderboardMessage extends Message {
+    private final boolean global;
+
     public GetLeaderboardMessage(String sender) {
+        this(sender, false);
+    }
+
+    public GetLeaderboardMessage(String sender, boolean global) {
         super(sender, MessageType.GET_LEADERBOARD);
+        this.global = global;
+    }
+
+    public boolean isGlobal() {
+        return global;
     }
 }
